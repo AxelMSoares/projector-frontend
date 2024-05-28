a533d5f57761f00196dac8307605e0ce977fb134
Axel <>
2024-05-28 16:10:27 +0200

    Verify if user is authorized before delete a project

--- src/components/project/projectDetails/deleteProjectBtn.jsx
+++ src/components/project/projectDetails/deleteProjectBtn.jsx
@@ -1,8 +1,30 @@
+import { useEffect } from "react";
 import { deleteProject } from "../../../api/deleteProject";
 
 export default function ({ jwt, projectUuid, project, userData }) {
 
+    useEffect(() => {
+        if (project) {
+            checkIfUserIsAuthorized();
+        }
+    }, [project]);
+
+    function checkIfUserIsAuthorized() {
+
+        if (userData.statut === "administrateur") {
+            return true;
+        }
+
+        return project.username === userData.username;
+    }
+
     async function deleteThisProject() {
+
+        // If the user is not the author of the project, we don't allow him to delete it
+        if (!jwt || !projectUuid || !checkIfUserIsAuthorized()){
+            return null;
+        }
+
         const confirmation = window.confirm("Êtes-vous sûr de vouloir supprimer ce projet?");
         if (confirmation) {
             try {
@@ -16,7 +38,7 @@ export default function ({ jwt, projectUuid, project, userData }) {
 
     return (
         <>
-            {project.username === userData.username ? (<button className="delete-project-btn" onClick={deleteThisProject}>Supprimer le projet</button>) : null}
+            {project.username === userData.username || userData.statut==="administrateur" ? (<button className="delete-project-btn" onClick={deleteThisProject}>Supprimer le projet</button>) : null}
         </>
     )
 }
