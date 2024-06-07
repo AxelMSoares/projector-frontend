bbec68aad6faf3252f2c4e750c2e373154ad2e4b
Axel <>
2024-06-07 16:05:41 +0200

    Changed footer. Added icons for socials

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -7,8 +7,14 @@ function Footer() {
                 <div className="footer-wrapper">
                     <ul>
                         <li><Link className="page-link" to="/politique-de-confidentialite">Politique de confidentialité</Link></li>
-                        <li><a href="">Qui Sommes Nous?</a></li>
-                        <li><a href="">Des avis? Des idées? Nous contacter</a></li>
+                        <li>
+                            <div className="icons-field">
+                                <a href="http://www.facebook.com" target="_blank"><img src="/icons/facebook.svg" /></a>
+                                <a href="http://www.instagram.com" target="_blank"><img src="/icons/instagram.svg" /></a>
+                                <a href="http://www.x.com" target="_blank"><img src="/icons/twitter-x.svg" /></a>
+                                <a href="http://www.discord.com" target="_blank"><img src="/icons/discord.svg" /></a>
+                            </div>
+                        </li>
                     </ul>
                 </div>
             </div>
